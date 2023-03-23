8545454d025437af9c120023c791af4ee6998c04
igor <igor@SPX5748.local>
2023-03-23 14:53:38 +0100

    feat: 1st commit

---
+++ lib/buttons/buttons.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  final String text;
+  const MyWidget({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    // Figma Flutter Generator ConfigurationfilledstateenabledshowiconfalseWidget - COMPONENT
+    return SizedBox(
+      width: 99,
+      height: 40,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              decoration: const BoxDecoration(),
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    text,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                        fontFamily: 'Roboto',
+                        fontSize: 14,
+                        letterSpacing: 0.10000000149011612,
+                        fontWeight: FontWeight.normal,
+                        height: 1.4285714285714286),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/vodafone_design_system.dart
@@ -0,0 +1,3 @@
+library vodafone_design_system;
+
+export './buttons/buttons.dart';
